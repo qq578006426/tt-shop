f8150c1651df131c70f6bbb12af8326fe539c412
刘晓行 <578006426@qq.com>
2019-04-03 17:37:35 +0800

    一次修改

--- src/main/java/com/itqf/TestSSM.java
+++ src/main/java/com/itqf/TestSSM.java
@@ -4,4 +4,8 @@ package com.itqf;
  * Created by刘晓行 on 2019/4/3 on 14:44
  */
 public class TestSSM {
+    public  static void main(String[] args){
+        System.out.println("test!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!");
+    }
 }
