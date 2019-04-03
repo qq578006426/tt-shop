356f56297ad6e5c14a0a896fe11ab596b9a4d7d8
刘晓行 <578006426@qq.com>
2019-04-03 17:40:23 +0800

    这是一次合并项目的测试

---
+++ src/main/java/com/itqf/TestZero.java
@@ -0,0 +1,10 @@
+package com.itqf;
+
+/**
+ * Created by刘晓行 on 2019/4/3 on 17:39
+ */
+public class TestZero {
+    public  static void main(String[] args){
+        System.out.println("这是一次合并项目的测试");
+    }
+}
